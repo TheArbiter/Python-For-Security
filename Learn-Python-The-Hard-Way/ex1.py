cf3d978f848a60df6d2538422aac78e8956c91b6
TheArbiter <ryan_ms28@hotmail.com>
2018-05-11 21:34:33 +1000

    add ex1.py

---
+++ Learn-Python-The-Hard-Way/ex1.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+#Study Drills
+print("From here onwards its study Drills")
+
+#Make python print only one line so we will store everything in a list
+myList = ["Hello World!", "Hello Again", "I like typing this.", "This is fun.", 'Yay! Printing.', "I'd much rather you 'not'.", 'I "said" do not touch this.']
+
+print(myList[1])
+
+#If I want to print 3 lines from the list I shall use a loop
+for i in myList[2:4]:
+    print(i)
